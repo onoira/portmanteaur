0ff573bec2cbd10ddf9b7b87a3b5b782755ea0c0
Onoira <onoira@psiko.zone>
2021-04-10 01:02:36 +0200

    nit: Spelling

--- portmanteaur/_portmanteaur.py
+++ portmanteaur/_portmanteaur.py
@@ -1,4 +1,4 @@
-"""portmanteau - Generate portmanteau for name generation"""
+"""portmanteaur - Generate portmanteau for name generation"""
 # Copyright (C) 2021 <onoira@psiko.zone>
 #
 # This program is free software: you can redistribute it and/or modify
